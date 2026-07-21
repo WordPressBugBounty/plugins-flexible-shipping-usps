58352f0e7ca731da62bb6d042c10da6c54d5287c
Sean Murphy <sean@iamseanmurphy.com>
2026-07-21 03:38:11 +0000

    3.5.1

--- flexible-shipping-usps/readme.txt
+++ flexible-shipping-usps/readme.txt
@@ -4,7 +4,7 @@ Contributors: octolize,grola,sebastianpisula
 Tags: usps, usps rates, usps shipping, usps woocommerce, usps live rates
 Requires at least: 5.7
 Tested up to: 7.0
-Stable tag: 3.5.0
+Stable tag: 3.5.1
 Requires PHP: 7.4
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -136,6 +136,9 @@ To determine which USPS services should be available for the customers to choose
 6. USPS Live Rates shipping methods in the cart
 
 == Changelog ==
+
+= 3.5.1 - 2026-07-20 =
+* Added support for WooCommerce 11.0
 
 = 3.5.0 - 2026-07-13 =
 * Added an AI chat assistant to USPS settings
--- flexible-shipping-usps/vendor/composer/installed.php
+++ flexible-shipping-usps/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'octolize-plugin/flexible-shipping-usps',
-        'pretty_version' => '3.5.0',
-        'version' => '3.5.0.0',
-        'reference' => '086d49ad26f317d7f37fc23f9de08a345563159e',
+        'pretty_version' => '3.5.1',
+        'version' => '3.5.1.0',
+        'reference' => '14e884e54c18641e0c9ca113e16951b6460df3ab',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'octolize-plugin/flexible-shipping-usps' => array(
-            'pretty_version' => '3.5.0',
-            'version' => '3.5.0.0',
-            'reference' => '086d49ad26f317d7f37fc23f9de08a345563159e',
+            'pretty_version' => '3.5.1',
+            'version' => '3.5.1.0',
+            'reference' => '14e884e54c18641e0c9ca113e16951b6460df3ab',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
